aeea77bf9a866e3f93d967d226b049794d5e1fd3
Gustavo Marques <gustavomarques.gml@gmail.com>
2020-12-13 11:45:27 -0300

    Bugfix on code

--- seq.hs
+++ seq.hs
@@ -1,6 +1,6 @@
 import System.Environment ( getArgs )   
 import Text.Printf ( printf )
-import Data.List.Split.Internals
+import Data.List.Split.Internals ( splitOn )
 
 help :: IO ()
 help = do
@@ -24,8 +24,8 @@ help = do
     printf "INCREMENTO geralmente é negativo se PRIMEIRO for maior que ÚLTIMO.\n"
     printf "INCREMENTO não deve ser 0; PRIMEIRO, INCREMENTO e ÚLTIMO não podem ser NaN.\n"
     printf "FORMATO deve estar apropriado para mostrar um argumento do tipo \"double\";\n"
-    printf "por padrão, é %.PRECf se PRIMEIRO, INCREMENTO e ÚLTIMO forem números\n"
-    printf "decimais de ponto fixo com precisão máxima PREC, e é %g caso contrário.\n\n"
+    printf "por padrão, é %%.PRECf se PRIMEIRO, INCREMENTO e ÚLTIMO forem números\n"
+    printf "decimais de ponto fixo com precisão máxima PREC, e é %%g caso contrário.\n\n"
     printf "Página de ajuda do GNU coreutils: <https://www.gnu.org/software/coreutils/>\n"
     printf "Relate erros de tradução do seq: <https://translationproject.org/team/pt_BR.html>\n"
     printf "Documentação completa em: <https://www.gnu.org/software/coreutils/seq>\n"
@@ -57,7 +57,7 @@ sizeNoLead n = sizeNoLeadFull n 0 1
 
 getPrecision :: [Char] -> Integer
 getPrecision n 
-    | null (tail (splitOn "." n)) = 0
+    | null (tail (splitOn "." n)) || null (head (tail (splitOn "." n))) = 0
     | otherwise = sizeNoLead (head (tail (splitOn "." n)))
 
 myDiv :: Integer -> Integer -> Double
@@ -65,7 +65,7 @@ myDiv a b =  (fromIntegral a :: Double)/(fromIntegral b :: Double)
 
 toValue :: [Char] -> Integer -> Integer
 toValue n precision
-    | null (tail (splitOn "." n)) = (10^precision)*(read (head (splitOn "." n)) :: Integer)
+    | null (tail (splitOn "." n)) || null (head (tail (splitOn "." n))) = (10^precision)*(read (head (splitOn "." n)) :: Integer)
     | otherwise = 
         (10^precision)*(read (head (splitOn "." n)) :: Integer) + (read (head (tail (splitOn "." n))) :: Integer)
 
@@ -74,36 +74,50 @@ isDouble st = not (null (tail st)) && (head st == '.' || isDouble (tail st))
 
 printSeqInt :: [Char] -> [Char] -> Bool -> Integer -> Integer -> Integer -> Integer -> IO ()
 printSeqInt f s w l inc r sz
+    | l > r = printf ""
     | w && f/="" = do
         printf "seq: o texto de formatação não pode ser especificado quando escrevendo textos de larguras iguais\n"
         printf "Tente \"seq --help\" para mais informações.\n"
-    | f/="" && l+inc > r = printf (f ++ "\n") (fromIntegral l :: Double)
+    | f/="" && l+inc > r = do
+        printf (f ++ "\n") (fromIntegral l :: Double)
+        printSeqInt f s w (l+inc) inc r sz
     | f/="" = do
         printf (f ++ "%s") (fromIntegral l :: Double) s
         printSeqInt f s w (l+inc) inc r sz
-    | w && l+inc > r = printf ("%0" ++ show sz ++ "d\n") l
+    | w && l+inc > r = do
+        printf ("%0" ++ show sz ++ "d\n") l
+        printSeqInt f s w (l+inc) inc r sz
     | w = do 
         printf ("%0" ++ show sz ++ "d%s") l s
         printSeqInt f s w (l+inc) inc r sz
-    | l+inc > r = printf "%d\n" l
+    | l+inc > r = do 
+        printf "%d\n" l
+        printSeqInt f s w (l+inc) inc r sz
     | otherwise = do
         printf "%d%s" l s
         printSeqInt f s w (l+inc) inc r sz
 
 printSeqFakeDouble :: [Char] -> [Char] -> Bool -> Integer -> Integer -> Integer -> Integer -> Integer -> IO ()
 printSeqFakeDouble f s w l inc r sz precision
+    | l > r = printf ""
     | w && f/="" = do
         printf "seq: o texto de formatação não pode ser especificado quando escrevendo textos de larguras iguais\n"
         printf "Tente \"seq --help\" para mais informações.\n"
-    | f/="" && l+inc > r = printf (f ++ "\n") l
+    | f/="" && l+inc > r = do
+        printf (f ++ "\n") (myDiv l (10^precision))
+        printSeqFakeDouble f s w (l+inc) inc r sz precision
     | f/="" = do
         printf (f ++ "%s") (myDiv l (10^precision)) s
         printSeqFakeDouble f s w (l+inc) inc r sz precision
-    | w && l+inc > r = printf ("%0" ++ show sz ++ "." ++ show precision ++ "f\n") (myDiv l (10^precision))
+    | w && l+inc > r = do
+        printf ("%0" ++ show sz ++ "." ++ show precision ++ "f\n") (myDiv l (10^precision))
+        printSeqFakeDouble f s w (l+inc) inc r sz precision
     | w = do 
         printf ("%0" ++ show sz ++ "."  ++  show precision ++  "f%s") (myDiv l (10^precision)) s
         printSeqFakeDouble f s w (l+inc) inc r sz precision
-    | l+inc > r = printf ("%." ++  show precision ++ "f\n") (myDiv l (10^precision))
+    | l+inc > r = do 
+        printf ("%." ++  show precision ++ "f\n") (myDiv l (10^precision))
+        printSeqFakeDouble f s w (l+inc) inc r sz precision
     | otherwise = do
         printf ("%." ++  show precision ++ "f%s") (myDiv l (10^precision)) s
         printSeqFakeDouble f s w (l+inc) inc r sz precision
@@ -124,20 +138,14 @@ printSeqHandler f s w l inc r
 printSeq :: [[Char]] -> [Char] -> [Char] -> Bool -> IO ()
 printSeq (arg:args) f s w
     -- One arg
-    | null args && (read arg :: Double) < 1 = 
-        printf ""
     | null args = 
         printSeqHandler f s w "1" "1" arg
         
     -- Two args
-    | null (tail args) && (read arg :: Double) > (read (head args) :: Double) = 
-        printf ""
     | null (tail args) = 
         printSeqHandler f s w arg "1" (head args)
     
     -- Tree args
-    |  null (tail (tail args)) && (read arg :: Double) > (read (head (tail args)) :: Double) = 
-        printf ""
     | null (tail (tail args)) = 
         printSeqHandler f s w arg (head args) (head (tail args))
     
